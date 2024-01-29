41ff8501a36f96937f203fb8fe369a90430c4b30
UWERA-Divine <84916731+UWERA-Divine@users.noreply.github.com>
2024-01-29 17:06:22 +0200

    updated dashboard -- addblog location

--- navigationControl.js
+++ navigationControl.js
@@ -1,9 +1,7 @@
 const addBlog = document.querySelector("#add__blog");
 const editBlog = document.querySelectorAll(".update");
 
-addBlog.onclick = () => {
-  window.location.href = "add_blog.html";
-};
+
 // editBlog.forEach((btn) => {
 //   btn.onclick = () => {
 //     window.location.href = "update_blog.html";
@@ -24,3 +22,7 @@ icons.forEach((icon, index) => {
       : "";
   };
 });
+
+addBlog.onclick = () => {
+  window.location.href = "add_blog.html";
+};
